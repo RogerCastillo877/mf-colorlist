96c0b5c56fd750979e7144f5deb7e23a7a9ad3e1
RogerCastillo877 <roger.castillo877@gmail.com>
2023-09-29 00:01:41 -0500

    add button

--- src/components/ColorList.jsx
+++ src/components/ColorList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Swal from 'sweetalert2';
 
-const ColorList = ({ colorList = [] }) => {
+const ColorList = ({ colorList = [], handleClickClearColors }) => {
 
   const handleCopyColor = (color) => {
 
@@ -18,30 +18,33 @@ const ColorList = ({ colorList = [] }) => {
   }
 
   return (
-    <div className='list-group text-center'>
-      {
-        colorList.length > 0
-          ? colorList.map((color, index) => (
-            <button
-              key={index}
-              type='button'
-              className='list-group-item list-group-item-action text-white'
-              aria-current="true"
-              title='Copiar'
-              style={{
-                background: color,
-                fontWeight: "bolder"
-              }}
-              onClick={() => handleCopyColor(color)}
-            >
-              {color}
-            </button>
-          ))
-          : <div className="alert alert-danger" role='alert'>
-            <b>Sin elementos por mostrar...</b>
-          </div>
-      }
-    </div>
+    <>
+      {colorList.length > 0 && (<button className='btn btn-danger my-4 w-100' onClick={handleClickClearColors}>Clear</button>)}
+      <div className='list-group text-center'>
+        {
+          colorList.length > 0
+            ? colorList.map((color, index) => (
+              <button
+                key={index}
+                type='button'
+                className='list-group-item list-group-item-action text-white'
+                aria-current="true"
+                title='Copiar'
+                style={{
+                  background: color,
+                  fontWeight: "bolder"
+                }}
+                onClick={() => handleCopyColor(color)}
+              >
+                {color}
+              </button>
+            ))
+            : <div className="alert alert-danger" role='alert'>
+              <b>Sin elementos por mostrar...</b>
+            </div>
+        }
+      </div>
+    </>
   );
 }
 
